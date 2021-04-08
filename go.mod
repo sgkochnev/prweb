f2ef24d740827d5f574477d0e6c959c7e3e3a2b7
sgkochnev <skochnev69@gmail.com>
2021-04-08 14:50:09 +0300

    add test

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module prweb
 go 1.16
 
 require (
-	github.com/go-chi/chi v1.5.4 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.4
+	github.com/ilyakaznacheev/cleanenv v1.2.5
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.3.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
---
+++ internal/handler/handler_test.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"prweb/internal/api"
+	"prweb/internal/api/mocks"
+	"testing"
+)
+
+func TestHandler_Hello(t *testing.T) {
+	tests := []struct {
+		name     string
+		joke     *api.JokeResponse
+		err      error
+		codeWant int
+		bodyWant string
+	}{
+
+		{
+			name:     "simple test",
+			joke:     &api.JokeResponse{Joke: "test joke"},
+			err:      nil,
+			codeWant: 200,
+			bodyWant: "test joke",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiMock := &mocks.Client{}
+			apiMock.On("GetJoke").Return(tt.joke, tt.err)
+
+			h := NewHandler(apiMock)
+			req, _ := http.NewRequest("GET", "/hello", nil)
+			rr := httptest.NewRecorder()
+
+			// handler := http.HandlerFunc(h.Hello)
+			// handler.ServeHTTP(rr, req)
+
+			h.Hello(rr, req)
+
+			gotRaw, _ := ioutil.ReadAll(rr.Body)
+
+			got := string(gotRaw)
+			if got != tt.bodyWant {
+				t.Errorf("wrong response body %s want %s", got, tt.bodyWant)
+			}
+			if status := rr.Result().StatusCode; status != tt.codeWant {
+				t.Errorf("wrong response status %d want %d", status, tt.codeWant)
+			}
+		})
+	}
+}
